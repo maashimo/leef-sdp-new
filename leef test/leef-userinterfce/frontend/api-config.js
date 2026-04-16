4f0e3ca92d608211b1b68879159a989be47e0adb
DISSANAYAKA D.M.M.P. IM/2022/122 <dissana-im22122@stu.kln.ac.lk>
2026-04-17 00:17:23 +0530

    UI Updates and Remove User Feature

--- leef test/leef-userinterfce/frontend/api-config.js
+++ leef test/leef-userinterfce/frontend/api-config.js
@@ -12,7 +12,8 @@
     // │       'http://142.93.208.192'                       │
     // │       'http://142.93.208.192:5000'                  │
     // └─────────────────────────────────────────────────────┘
-    const BACKEND_URL = "https://142.93.208.192.sslip.io";
+    // const BACKEND_URL = "https://142.93.208.192.sslip.io";
+    const BACKEND_URL = "http://192.168.1.9:5000";
 
     window.API_BASE_URL = BACKEND_URL;
     console.log(`%c[Leef Config] Using API: ${BACKEND_URL}`, "color: #4ade80; font-weight: bold;");
